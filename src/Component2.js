2aa7751d86f1f8244a0f9861e6a1da0f8b3abb1b
Praj122 <132545431+Praj122@users.noreply.github.com>
2025-06-14 20:12:43 +0530

    Update Component2.js

--- src/Component2.js
+++ src/Component2.js
@@ -1,18 +1,16 @@
 import React from 'react'
 import './component2.css'
-import toast, { Toaster } from 'react-hot-toast';
 function Component2({imageUrl,name,age,country,content})
 {
     return(
     <div>
-        <div><Toaster/></div>
     <div className="container">
     <img src={imageUrl} height={250} width={250}></img>
     <p><strong>Name: </strong>{name}</p>
     <p><strong>Age: </strong>{age}</p>
     <p><strong>Gender: </strong> Male</p>
     <p><strong>Country: </strong>{country}</p>
-    <button onClick={() => toast(content)} onMouseOver={()=>toast.success('Click me!!')}>Load more</button>
+    <button onClick={() => alert(content)}>Load more</button>
     </div>
     </div>
     )
